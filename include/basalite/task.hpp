ea786c00f4fd0186dac36e2193d97f33aa510cc8
Zffu <103074097+Zffu@users.noreply.github.com>
2025-11-19 15:59:45 +0100

    feat: added basic class defs

---
+++ include/basalite/task.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file task.hpp
+ * @details Task related definitions
+ */
+
+#pragma once
+
+#include <string>
+
+namespace basalite {
+
+class task {
+private:
+
+public:
+	std::string name;
+
+	task(std::string name);
+};
+
+}
+
